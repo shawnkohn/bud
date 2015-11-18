cf308c19f06de5f0e374c4de8ece60912e65bab4
Shawn Kohn <shawnkohn@gmail.com>
2015-11-18 14:22:24 -0500

    added some test functionality

--- test/controllers/paychecks_controller_test.rb
+++ test/controllers/paychecks_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PaychecksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    include Devise::TestHelpers
+
+    setup :initialize_paycheck
+    
+    test "should get index" do
+        get :index, :format => :json, user_id: @user
+        assert_response :success
+    end
+
+
+    private
+
+    def initialize_paycheck
+        @user = users(:one)
+        sign_in @user
+        @paycheck = paychecks(:one)
+    end
+
 end
